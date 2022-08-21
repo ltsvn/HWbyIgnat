05c302bc26d74cbcf072552f5bf72097cbba1dcb
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-08-21 09:48:46 +0200

    First hm - Make a massage

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Message from "./Message";
+
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    name: 'Artem',
+    message: 'npm start нажимал?',
     time: '22:00',
 }
 
@@ -11,20 +13,24 @@ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
+            {/*homeworks 1*/}
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
             <hr/>
         </div>
     )
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import s from './Message.module.css'
+export type messagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
 
-function Message() {
+function Message({avatar, name, message, time}: messagePropsType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <img className={s.avatar} src={avatar} alt={'Avatar'}/>
+            <div className={s.content}>
+                <div className={s.name}>
+                    {name}
+                </div>
+                <div className={s.text}>
+                    {message}
+                </div>
+                <div className={s.time}>
+                    {time}
+                </div>
+            </div>
         </div>
     )
 }
