94f2f16c73ea0b0d41c072dde18d9776b1826da8
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-09-16 19:26:04 +0200

    Updates

---
+++ precache-manifest.8b64891b9a6f6b55a60e3d480ab4c4ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ac6ef1bf2c976fea97fb03222bb572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbec2e1f93ec2baa3e6a",
+    "url": "/static/css/main.4c6b6c2e.chunk.css"
+  },
+  {
+    "revision": "32c3e359a0669e8103a6",
+    "url": "/static/js/2.7bf32c7d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7bf32c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbec2e1f93ec2baa3e6a",
+    "url": "/static/js/main.ab963993.chunk.js"
+  },
+  {
+    "revision": "b8c053cb2522392dd525",
+    "url": "/static/js/runtime-main.d9469fe2.js"
+  }
+]);
