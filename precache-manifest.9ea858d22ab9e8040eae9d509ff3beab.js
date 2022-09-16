3617bbed8ac42293eb65593c2b11709fb94f6535
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-09-16 19:40:45 +0200

    Updates

---
+++ precache-manifest.9ea858d22ab9e8040eae9d509ff3beab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86ffd6afb2cf4cf6e58b1f1bce5c510",
+    "url": "/HWbyIgnat/index.html"
+  },
+  {
+    "revision": "1b8054ea2341c74ea2a8",
+    "url": "/HWbyIgnat/static/css/main.9744356f.chunk.css"
+  },
+  {
+    "revision": "c130527ca1b87479b30e",
+    "url": "/HWbyIgnat/static/js/2.d424e6a1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/HWbyIgnat/static/js/2.d424e6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8054ea2341c74ea2a8",
+    "url": "/HWbyIgnat/static/js/main.6ccd224c.chunk.js"
+  },
+  {
+    "revision": "d00a22a0c5c9b4521e1a",
+    "url": "/HWbyIgnat/static/js/runtime-main.846317df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b64891b9a6f6b55a60e3d480ab4c4ef.js"
+  "/HWbyIgnat/precache-manifest.9ea858d22ab9e8040eae9d509ff3beab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HWbyIgnat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
